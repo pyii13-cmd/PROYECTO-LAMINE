cff9ea63014e9110567bd04602925c4616b1f29e
pyii13-cmd <pyrjh13@gmail.com>
2025-11-05 14:30:44 +0100

    Add files via upload

--- ImageFiles.py
+++ ImageFiles.py
@@ -38,4 +38,33 @@ class ImageFiles:
     def reload_fs(self,path: str):
         self._arxius_anteriors = self._arxius_actuals.copy()
         nous_arxius = set()
-        
+        for dir, sdir, arxius in os.walk(path):
+            for nom_arxiu in arxius:
+                if nom_arxiu.lower().endswith('.png'):
+                    path_complet = os.path.join(dir,nom_arxiu)
+                    path_relatiu = cfg.get_canonical_pathfile(path_complet) # construye el path desde donde tu estas hasta el archivo que tu quieres 
+                    nous_arxius.add(path_relatiu)
+        self._arxius_actuals = nous_arxius
+
+    def files_added(self):
+        afegides = self._arxius_actuals - self._arxius_anteriors
+        return list(afegides)
+    
+    def files_removed(self):
+        eliminades = self._arxius_anteriors - self._arxius_actuals
+        return list(eliminades)
+    
+    def __len__(self) -> int:
+        """
+        Retorna el nombre d'arxius PNG gestionats actualment.
+        """
+        # Simplement retornem la mida del nostre conjunt d'arxius actuals
+        return len(self._arxius_actuals)
+
+    def __str__(self) -> str:
+        """
+        Retorna una representació en text de l'objecte.
+        """
+        # Podem reutilitzar el nostre propi __len__!
+        quantitat = len(self)
+        return f"<ImageFiles: gestionant {quantitat} arxius PNG>"
